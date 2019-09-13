cff13e6ae6434282fd8973e2180e3d5aef1b940d
rudy3199 <rudy31@hotmail.es>
2019-09-13 16:51:48 -0600

    Primo_Corregido.py
    Se corrigieron los problemas

--- Primos_malo.py
+++ Primos_malo.py
@@ -1,18 +1,25 @@
-def primos(a):#define funcion
-    if a < 1 :#condicional para detectar si a es menor a 1
+def primos(a):
+    # define funcion
+    if a < 1:
+        # condicional para detectar si a es menor a 1
         print('Rango no válido')
-    elif a == 1:#condicional para detectar si a es igual a 1
+    elif a == 1:
+        # condicional para detectar si a es igual a 1
         print('Fin')
-    else:#si no se cumple ninguna condicion se
+    else:
+        # si no se cumple ninguna condicion se
         count = 0
-        for i in range(1,a+1):#se realiza un for
-            if a%i != 0:
+        for i in range(1, a+1):
+            # se realiza un for
+            if a % i != 0:
                 pass
             else:
-                countant = count
+                count = count
                 count = count + 1
-if count > 2:#condicional para determinar si el contador es mayor a 2
+    if count > 2:
+        # condicional para determinar si el contador es mayor a 2
             primos(a-1)
-        else:
-            print(a)#imprime numero a
-            primos(a -1)
+    else:
+            print(a)
+            # imprime numero a
+            primos(a-1)
